cccc25f3f0cbc8281292f50c8b7ed1acf8be6419
Basmala Mohamed Zain El Abidine <102691187+BasmalaMohamed46@users.noreply.github.com>
2024-06-14 00:06:33 +0300

    feat:add setEmail and getEmail test function

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -11,7 +11,7 @@ class UserTest extends TestCase
 
     protected function setUp(): void
     {
-        $this->user = new User('Belal Abo Ata', 'belal@gmail.com');
+        $this->user = new User('Belal Abo Ata', 'basmala.mohamed.zain@gmail.com');
     }
 
     public function testGetName()
@@ -23,5 +23,16 @@ class UserTest extends TestCase
     {
         $this->user->setName('Noha');
         $this->assertEquals('Noha', $this->user->getName());
+    }
+
+    public function testGetEmail()
+    {
+        $this->assertEquals('basmala.mohamed.zain@gmail.com', $this->user->getEmail());
+    }
+
+    public function testSetEmail()
+    {
+        $this->user->setEmail('nohasalah@iti.gov.eg');
+        $this->assertEquals('nohasalah@iti.gov.eg', $this->user->getEmail());
     }
 }
